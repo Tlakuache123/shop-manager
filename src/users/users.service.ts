561598df527adfff03c05893f4b01e606606e21e
Claudio Araujo <77213711+Tlakuache123@users.noreply.github.com>
2024-10-13 20:27:28 -0600

    auth add: mockup of authenticated an user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
+const users: User[] = [
+  {
+    userId: 1,
+    username: 'Alice',
+    password: 'topsecret',
+  },
+  {
+    userId: 2,
+    username: 'Bob',
+    password: '123',
+  },
+];
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  findUserByName = async (username: string): Promise<User | undefined> => {
+    return users.find((user) => user.username === username);
+  };
+}
